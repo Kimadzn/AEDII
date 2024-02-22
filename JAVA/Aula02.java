0b45a7fabcec29dc4231f359efa0207bf295654b
Arthur <kimadzn@gmail.com>
2024-02-22 14:13:03 -0300

    .

---
+++ JAVA/Aula02.java
@@ -0,0 +1,13 @@
+public class Aula02 {
+    public static void main (String[] args){
+        int num1, num2;
+        String s = " string ";
+        int i = 100 ;
+
+        num1 = 100;
+        num2 = num1 + 100;
+        System.out.println("O resultado é: " + num2);
+        System.out.println("Num1 é igual: " + num1);
+
+    }
+}
